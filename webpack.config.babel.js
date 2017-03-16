cfaa85b9f2aa83d4a2d6ca6110af2c4064421c0d
rama99 <rama.kishore@gmail.com>
2017-03-16 23:29:03 +0530

    webpack changes

---
+++ webpack.config.babel.js
@@ -0,0 +1,23 @@
+import {join} from 'path'
+
+const include = join(__dirname,'src')
+
+export default {
+
+entry: './src/index',
+output: {
+    path: join(__dirname , 'dist'),
+    libraryTarget:'umd',
+    library:'twinconsole',
+},
+devtool: 'source-map',
+module: {
+    loaders:[
+{test: /\.js$/ , loader: 'babel-loader' , include},
+{test: /\.json$/ , loader: 'babel-loader' , include}
+
+    ]
+}
+
+
+}
